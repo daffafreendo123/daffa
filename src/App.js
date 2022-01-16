634ae44882abac1660070873d9f47b06793987ad
daffafreendo123 <97815786+daffafreendo123@users.noreply.github.com>
2022-01-15 23:56:23 -0800

    read masseh

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  let data1 = 0;
+
+  const data2 = () => {};
+
+  function name(params) {}
+
+  return (
+    <div className="container">
+      <div className="Section">
+        <div className="card section1">1</div>
+        <div className="card section2">2</div>
+        <div className="card section3">3</div>
+        <div className="card section4">4</div>
+        <div className="card section5">5</div>
+        <div className="card section6">6</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
